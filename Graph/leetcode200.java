8ff0eafd372ec3a0f367f726e9525ad50b71f96f
Jaswanth <jaswanthramesh24@gmail.com>
2026-03-14 22:19:33 +0530

    Implement numIslands and DFS for island counting

---
+++ Graph/leetcode200.java
@@ -0,0 +1,22 @@
+public int numIslands(char[][] grid) {
+        int cnt=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return cnt;
+    }
+    public void dfs(char[][]grid,int i,int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='.' || grid[i][j] != '1')
+        return;
+        grid[i][j]='.';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+}
