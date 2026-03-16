d6598681c997008c2c0dd658ca7e63281ce3fda9
Jaswanth <jaswanthramesh24@gmail.com>
2026-03-16 22:13:04 +0530

    Add topKFrequent method to Solution class
    Implement topKFrequent method to find k most frequent elements.

--- heap/leetcode347.java
+++ heap/leetcode347.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->map.get(a)-map.get(b));
+        for(int key:map.keySet()){
+            pq.offer(key);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int res[]=new int[k];
+        int i=0;
+        while(!pq.isEmpty()){
+            res[i]=pq.poll();
+            System.out.println(res[i]);
+            i++;
+        }
+        return res;
+    }
+}
