bfb3d627463ffd89117e11311a0ddd7cc3fc4c3e
Jaswanth <jaswanthramesh24@gmail.com>
2026-03-17 14:10:10 +0530

    Implement lengthOfLIS function for longest increasing subsequence

---
+++ dynamicprogramming/leetcode300.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int dp[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            dp[i]=1;
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i] && dp[i]<dp[j]+1){
+                    dp[i]=dp[j]+1;
+                }
+            }
+        }
+        int max=dp[0];
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(dp[i],max);
+        }
+        return max;
+    }
+}
