8e199e4bf3682af993f11c5bc9d515e170a71237
Jaswanth <jaswanthramesh24@gmail.com>
2026-06-25 23:40:41 +0530

    Fix formatting and syntax issues in leetcode 18.java

--- array/leetcode 18.java
+++ array/leetcode 18.java
@@ -6,7 +6,7 @@ class Solution {
             return res;
         Arrays.sort(nums);
         for(int i=0;i<n-3;i++)
-        {
+        {   
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
             for(int j=i+1;j<n-2;j++)
