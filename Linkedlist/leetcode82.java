26b4bce0ded03484d491e000e107b6e07468ce04
Jaswanth <jaswanthramesh24@gmail.com>
2026-07-24 23:37:36 +0530

    Refactor deleteDuplicates method for clarity

--- Linkedlist/leetcode82.java
+++ Linkedlist/leetcode82.java
@@ -10,23 +10,24 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null){return head;}
-        ListNode dummy=new ListNode(0);
-        dummy.next=head;
-        ListNode previous=dummy;
-        ListNode current=head;
-        while(current != null && current.next != null){
-            if(current.val==current.next.val){
-                while(current.next != null && current.val == current.next.val){
-                    current=current.next;
-                }
-                previous.next=current.next;
+       if (head == null || head.next == null) return head;
+       ListNode dummy=new ListNode(0);
+       dummy.next=head;
+       ListNode previous=dummy;
+       ListNode current =head;
+       while(current != null && current.next != null){
+        if (current.val == current.next.val){
+            while(current.next !=null && current.val == current.next.val){
+                current = current.next;
             }
-            else{
-                previous=previous.next;
-            }
-            current=current.next;
+            previous.next=current.next;
+
         }
+        else{
+            previous=previous.next;
+        }
+        current=current.next;
+       }
         return dummy.next;
     }
 }
