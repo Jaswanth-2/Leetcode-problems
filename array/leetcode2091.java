54bca2fe90b8b5ddadb25957baa9724704340f2a
Jaswanth <jaswanthramesh24@gmail.com>
2026-08-30 23:42:07 +0530

    Implement minimumDeletions method in Solution class

---
+++ array/leetcode2091.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+        int n = nums.length;
+
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minIndex]) {
+                minIndex = i;
+            }
+
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+
+        int l = Math.min(minIndex, maxIndex);
+        int r = Math.max(minIndex, maxIndex);
+
+        return Math.min(
+            Math.min(r + 1, n - l),
+            l + 1 + n - r
+        );
+    }
+}
